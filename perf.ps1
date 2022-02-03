7d93c4607608139267e2e85467391e0170d8878f
martincostello <martin@martincostello.com>
2022-02-03 15:09:46 +0000

    Add script to run crank
    Add script to run crank on the sandbox app.

---
+++ perf.ps1
@@ -0,0 +1,30 @@
+#! /usr/bin/env pwsh
+
+#Requires -PSEdition Core
+#Requires -Version 7
+
+param(
+    [Parameter(Mandatory = $false)][string] $Scenario = "root",
+    [Parameter(Mandatory = $false)][string] $Profile = "local",
+    [Parameter(Mandatory = $false)][string] $BranchOrCommitOrTag = ""
+)
+
+$additionalArgs = @()
+
+if (![string]::IsNullOrEmpty($BranchOrCommitOrTag)) {
+    $additionalArgs += "--application.source.branchOrCommit"
+    $additionalArgs += $BranchOrCommitOrTag
+}
+
+$repoPath = Split-Path $MyInvocation.MyCommand.Definition
+$config = Join-Path $repoPath "crank.yml"
+
+$agent = crank-agent &
+
+try {
+    crank --config $config --scenario $Scenario --profile $Profile $additionalArgs
+}
+catch {
+    Stop-Job -InstanceId $agent.InstanceId
+    Wait-Job -InstanceId $agent.InstanceId
+}
