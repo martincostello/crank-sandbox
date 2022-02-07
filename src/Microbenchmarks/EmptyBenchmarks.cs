09993a5925b534f310f26c7fbfd3c73bbbcde541
martincostello <martin@martincostello.com>
2022-02-07 10:49:09 +0000

    Add a third benchmark
    Add a third benchmark that uses a baseline.

---
+++ src/Microbenchmarks/EmptyBenchmarks.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using BenchmarkDotNet.Attributes;
+
+namespace Microbenchmarks;
+
+public class EmptyBenchmarks
+{
+    [Benchmark(Baseline = true)]
+    public string[] ArrayEmpty() => Array.Empty<string>();
+
+    [Benchmark]
+    public object EnumerableEmpty() => Enumerable.Empty<string>();
+}
