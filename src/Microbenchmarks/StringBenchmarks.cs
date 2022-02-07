100880a8c463b3454c72b52ce517ec3f8861c204
martincostello <martin@martincostello.com>
2022-02-07 10:59:49 +0000

    Add another benchmark
    Add another benchmark with a baseline that actually allocates.

---
+++ src/Microbenchmarks/StringBenchmarks.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using BenchmarkDotNet.Attributes;
+
+namespace Microbenchmarks;
+
+public class StringBenchmarks
+{
+    private static readonly char[] TwentySpaces = Enumerable.Repeat(' ', 20).ToArray();
+
+    [Benchmark(Baseline = true)]
+    public string ConstructorArray() => new string(TwentySpaces);
+
+    [Benchmark]
+    public string ConstructorSpan() => new string(TwentySpaces.AsSpan());
+}
