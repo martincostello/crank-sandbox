1e9ba0983220eb46d8bc6befc3b83be27e5f6efa
martincostello <martin@martincostello.com>
2022-02-07 10:43:56 +0000

    Add another benchmark
    Add a second benchmark class.

--- src/Microbenchmarks/EnumerableBenchmarks.cs
+++ src/Microbenchmarks/EnumerableBenchmarks.cs
@@ -1,6 +1,12 @@
 ﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-BenchmarkDotNet.Running.BenchmarkSwitcher
-    .FromAssembly(typeof(Program).Assembly)
-    .Run(args);
+using BenchmarkDotNet.Attributes;
+
+namespace Microbenchmarks;
+
+public class EnumerableBenchmarks
+{
+    [Benchmark]
+    public object Empty() => Enumerable.Empty<string>();
+}
